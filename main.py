3a3e037c5f46b32e9e42388240e3ea2349c19f9d
braden <leachbr787@northwested.org>
2025-04-21 14:17:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,18 +41,20 @@ ICON = "C:\\Project-Text-Menu\\favicon-starter-cropped.png"
     # - Sounds - #
 ROBLOX_DEATH_SOUND = "C:\\Project-Text-Menu\\roblox-death-sound.ogg"
 LAZER_SOUND = "C:\\Project-Text-Menu\\laser5.ogg"
-ZAP_SOUND = "C:\\Project-Text-Menu\\zap13.ogg"
 
 
+# -- Intilize -- # 
 def init_game (): 
     pygame.init()
     pygame.font.init()
     pygame.mixer.init()  # Initialize the mixer
+    pygame.mixer.music.load("C:\Project-Text-Menu\epic-skies-SBA-348586748-preview.mp3") # Load Music
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) # Use constants from config
     pygame.display.set_caption(TITLE)
     return screen
 
 
+# -- Events -- # 
 def handle_events ():
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
@@ -63,27 +65,75 @@ def handle_events ():
     return True
 
 
+# -- Draw Text -- # 
+def draw_text(screen, text, font, text_color, y, bold=False, italic = False): 
+   font.set_bold(bold)
+   font.set_italic(italic)
+
+   text_surface = font.render(text, True, text_color)
+   screen.blit(text_surface, (90, y))
+
+
 def main():
    screen = init_game()
    clock = pygame.time.Clock() # Initialize the clock here
 
-
-    # -- Load Sounds -- # 
+   # -- Load Sounds -- # 
    roblox_soundL = pygame.mixer.Sound(ROBLOX_DEATH_SOUND)
    lazer_soundL = pygame.mixer.Sound(LAZER_SOUND)
-   zap_soundL = pygame.mixer.Sound(ZAP_SOUND)
 
+
+   # -- Font -- # 
+   text_font = pygame.font.Font(TEXT_FONT, 30)
+
+   # -- Menu -- # 
+   Menu = ["Press 'a' to play Sound Effect #1", "Press 's' to play Sound Effect #2", "Press 'b' to play background music", "Press 'p' to pause the background music", "Press 'f' to unpause the background music", "Press 'r' to generate a random text color", "Press 'u' to change the background"]
+
+   # -- Offset -- # 
+   base_y = 30 # starting y-value for the top of menu
+   line_height = 60 # Number of pixels between each text
+
+
+   # -- Load and Set Up Graphic -- #
+   background_image = pygame.image.load("C:\Project-Text-Menu\halloween-scary-ghost-face-illustration-cartoon-hd-png-701751694865357gijwaaz3fi-removebg-preview.png").convert()
+   background_image.set_colorkey(BLACK)
 
    running = True
    while running:
       running = handle_events()
-      # -- -- #
-
-
 
       screen.fill(WHITE) # Use color from config
 
-      # -- -- #
+      # -- If Key Pressed -- # 
+      keys = pygame.key.get_pressed()
+      if keys[pygame.K_a]:
+         roblox_soundL.play()
+      if keys[pygame.K_s]:
+         lazer_soundL.play()
+      if keys[pygame.K_b]:
+         pygame.mixer.music.play()
+      if keys[pygame.K_p]:
+         pygame.mixer.music.pause()
+      if keys[pygame.K_f]:
+         pygame.mixer.music.unpause()
+      if keys[pygame.K_r]:
+         random_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+         text_color = random_color
+      else: 
+         text_color = GREEN
+      if keys[pygame.K_u]:
+         screen.blit(background_image, [200,200])
+      
+
+      # -- Move Through Menu -- #
+      for i in range(len(Menu)): 
+         draw_text(screen, Menu[i], text_font, text_color, base_y + i * line_height)
+
+
+      # -- Loop Through For loop -- # 
+      for event in pygame.event.get(): 
+         if event.type == pygame.QUIT: 
+            running = False
 
 
       pygame.display.flip()
